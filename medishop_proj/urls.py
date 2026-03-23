89243aa28b8b7ee26b12f1e2ca270cd06885ddc8
dhruv chavda <dhruvchavda447@gmail.com>
2026-03-23 08:18:04 +0530

    add create admin route

--- medishop_proj/urls.py
+++ medishop_proj/urls.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth.models import User
+# After deploy, visit /create-admin/ once to create superuser
+from django.http import HttpResponse
+def create_admin(request):
+    if not User.objects.filter(username='dhruv').exists():
+        User.objects.create_superuser('dhruv','dhruvchavda.intern@gmail.com','Admin@1234')
+        return HttpResponse('Admin created! username: dhruv password: Admin@1234')
+    return HttpResponse('Already exists')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home_app.urls')),
@@ -12,4 +19,5 @@ urlpatterns = [
     path('', include('skin_ai.urls')),
     path('', include('sentiment_check.urls')),
     path('', include('portfolio.urls')),
+path('create-admin/', create_admin),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
